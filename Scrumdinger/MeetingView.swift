cf576ca7cb956c5866e8612c41d3e6351a0b0110
JMichaelLima <jml@jmlima.com>
2023-10-26 12:10:51 -0500

    edited filename

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by JM Lima on 10/24/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        ProgressView(value:5,total:15)
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
+/*
+#Preview {
+    ContentView()
+}
+*/
